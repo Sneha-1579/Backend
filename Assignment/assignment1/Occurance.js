894a922a0c835757a8abe7dafefbe38f7714bae4
Sneha Tripathi <snehatripathi1002@gmail.com>
2026-03-22 18:37:39 +0530

    completed

---
+++ Assignment/assignment1/Occurance.js
@@ -0,0 +1,13 @@
+const arr = ['a', 'b', 'c'];
+const target = 'b';
+
+let index = -1;
+
+for(let i = 0; i < arr.length; i++){
+  if(arr[i] === target){
+    index = i;
+    break;
+  }
+}
+
+console.log(index);
